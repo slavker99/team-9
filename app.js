6b1f89d86350032d376c130546bb7d79ce70f811
slavker99 <48331491+slavker99@users.noreply.github.com>
2020-03-21 22:50:03 +0500

    4

--- app.js
+++ app.js
@@ -5,7 +5,4 @@ app.get("/", function(request, response){
 	response.send("<br>РИ-370005 <br/> <br> Тема: доска задач </br> <br> 1. Кузнецов Максим(капитан) <br/> <br> 2. Огаров Дмитрий <br/> <br> 3. Пенягин Святослав <br/> <br> 4.Голубев Алексей <br/> <br> 5.Дмитрий Кувашов <br/>");
 });
 
-var port = process.env.PORT || 3000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+module.exports = app;
