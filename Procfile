135dc1203a81f030168ee9ae7d8d99edc2febeb8
slavker99 <48331491+slavker99@users.noreply.github.com>
2020-03-21 22:30:01 +0500

    init

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: node www.js
+web: node app.js
--- app.js
+++ app.js
@@ -5,4 +5,7 @@ app.get("/", function(request, response){
 	response.send("<br>РИ-370005 <br/> <br> Тема: доска задач </br> <br> 1. Кузнецов Максим(капитан) <br/> <br> 2. Огаров Дмитрий <br/> <br> 3. Пенягин Святослав <br/> <br> 4.Голубев Алексей <br/> <br> 5.Дмитрий Кувашов <br/>");
 });
 
-module.exports = app;
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
