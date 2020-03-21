6b21bc7ba27be0905f1cfc9fab7b82c6ba9fda52
slavker99 <48331491+slavker99@users.noreply.github.com>
2020-03-21 22:47:03 +0500

    3

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const app = express();
-app.use(express.logger());
 
 app.get("/", function(request, response){
 	response.send("<br>РИ-370005 <br/> <br> Тема: доска задач </br> <br> 1. Кузнецов Максим(капитан) <br/> <br> 2. Огаров Дмитрий <br/> <br> 3. Пенягин Святослав <br/> <br> 4.Голубев Алексей <br/> <br> 5.Дмитрий Кувашов <br/>");
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 3000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
