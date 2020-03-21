d875083314250efb2ef55c17636ddf7667a7aeb7
slavker99 <48331491+slavker99@users.noreply.github.com>
2020-03-21 22:33:04 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+app.use(express.logger());
 
 app.get("/", function(request, response){
 	response.send("<br>РИ-370005 <br/> <br> Тема: доска задач </br> <br> 1. Кузнецов Максим(капитан) <br/> <br> 2. Огаров Дмитрий <br/> <br> 3. Пенягин Святослав <br/> <br> 4.Голубев Алексей <br/> <br> 5.Дмитрий Кувашов <br/>");
